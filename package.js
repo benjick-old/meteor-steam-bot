0c43319b152fccdb5e63f203111053817e8f29d7
Max Malm <benjick@dumfan.net>
2015-03-20 16:03:39 +0100

    Update package.js

--- package.js
+++ package.js
@@ -1,15 +1,17 @@
 Package.describe({
 	summary: 'Lets you interface with Steam without running an actual Steam client',
-	version: '0.6.7',
-	name: 'maxpain:steam-bot',
-	git: 'https://github.com/Maxpain177/meteor-steam-bot.git'
+	version: '0.0.1',
+	name: 'benjick:steam-bot',
+	git: 'https://github.com/benjick/meteor-steam-bot.git'
 });
 
 Package.onUse(function(api) {
-	api.export('SteamBot', 'server');
+	api.export('Steam', 'server');
+	api.export('SteamTradeOffers', 'server');
 	api.addFiles(['lib/steam-bot.js'], 'server');
 });
 
 Npm.depends({
-	steam: '0.6.7'
+	steam: '0.6.7',
+	steam-tradeoffers: '1.2.1'
 });
